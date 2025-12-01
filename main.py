880549e953899a563809b48756d5480c1ed53748
msnape90 <msnape90@gmail.com>
2025-12-01 16:21:41 -0500

    refomatted main to print report

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from stats import get_num_words
+from stats import get_num_words,  get_book_text
 from stats import get_character_count
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    get_num_words(book_text)
-    get_character_count(book_text)
 
-def get_book_text(path_to_book):
-    with open(path_to_book) as f:
-        file_contents = f.read()
-    return(file_contents)
+    book_location = "books/frankenstein.txt"
+
+    main_header = "============ BOOKBOT ============"
+    main_footer = "============= END ===============" 
+    word_count_header = "----------- Word Count ----------"
+    char_count_header = "--------- Character Count -------"
+
+    book_text = get_book_text(book_location)
+
+    analyze_statement = f"Analyzing book found at {book_location}..."
+    word_count_statement = get_num_words(book_text)
+
+    char_count = get_character_count(book_text)
+
+    print(f"{main_header}\n{analyze_statement}\n{word_count_header}\n{word_count_statement}\n{char_count_header}\n{main_footer}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,14 @@
 
+
+def get_book_text(path_to_book):
+    with open(path_to_book) as f:
+        file_contents = f.read()
+    return(file_contents)
+
 def get_num_words(text_from_book):
     words_from_book = text_from_book.split()
     num_words = len(words_from_book)
-    print(f"Found {num_words} total words")
+    return f"Found {num_words} total words"
 
 def get_character_count(text_from_book):
 
@@ -17,5 +23,6 @@ def get_character_count(text_from_book):
             else:
                 # print(f"{lower_character} added")
                 characters_in_book[lower_character] = 1
-    print(characters_in_book) 
+    # print(characters_in_book) 
+    return(characters_in_book)
 
