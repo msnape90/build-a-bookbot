40aa1fcf136ad5d4659504616311b013c821409c
msnape90 <msnape90@gmail.com>
2025-12-03 11:43:54 -0500

    added arguments, exit code

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import sys
 from stats import get_num_words,  get_book_text, printable_sorted_char_count, sort_char_count
 from stats import get_char_count
 
 def main():
-    print_report("books/frankenstein.txt")
+    arguments = sys.argv
+    try:
+        book_location = arguments[1]
+        print_report(book_location)
+    except IndexError:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 def print_report(book_location):
-
+    if book_location == None:
+        print("Please provide the relative location of the book you would like a report on")
     main_header = "============ BOOKBOT ============"
     main_footer = "============= END ===============" 
     word_count_header = "----------- Word Count ----------"
